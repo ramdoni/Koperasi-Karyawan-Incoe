e27f1f34588eb242052a940a987ec5207331c1e6
Ramdoni <doni.enginer@gmail.com>
2022-11-06 10:19:37 +0700

    anggota,shu page

--- backend/src/App.js
+++ backend/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Topnavbar from './TopNavbar';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link} from "react-router-dom";
+import Anggota from './Anggota';
+import Shu from './Shu';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="hk-wrapper" data-layout="navbar" data-layout-style="default" data-menu="light" data-footer="simple">
+      <Router>
+        <Topnavbar />
+        <Routes>
+            <Route path="/anggota" element={<Anggota/>} />
+            <Route path="/shu" element={<Shu/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
